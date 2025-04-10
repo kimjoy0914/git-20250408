01b123a71caaae5f16ad7d4e80aaf50a340bb542
kimjoy0914 <kimjoy0914@gmail.com>
2025-04-10 09:08:54 +0900

    충돌노트추가

--- basic-syntax/basic-command.sh
+++ basic-syntax/basic-command.sh
@@ -10,3 +10,13 @@ git pull origin 브랜치명
 # 원격에 변경 사항을 local에 가져오되 병합은 하지 않는 것 
 git fetch origin 브랜치명
 git merge origin/브랜치명 #이 때는 origin과 브랜치명 사이에 슬래쉬 할것.
+
+#  git 작업 취소
+# commit 이후의 취소
+git reset head~1
+git reset head^
+# staging area로 까지만 취소
+git reset --soft head~1
+
+# push 이후의 취소 -> git commit메시지 작성 vi화면으로 이동 -> 수정 후 화면 닫기
+git revert 기존의 커밋ID
